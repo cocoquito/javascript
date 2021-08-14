99e078b0a3da64189501d2764e8bb3192e1ea64e
cocoquito <39251706+cocoquito@users.noreply.github.com>
2021-08-14 04:12:58 -0700

    Create script.js

---
+++ EjemploIF/script.js
@@ -0,0 +1,11 @@
+function sale(){
+      var foo= document.getElementById("uno").value;
+      if(foo>=51){
+        document.getElementById("re").innerHTML = "Aprobado";
+      }else{
+        document.getElementById("re").innerHTML = "Reprobado";
+      }
+}
+function goo(){
+  document.getElementById("dosss").innerHTML = "Pagina cargada exitosamente ...";
+}
